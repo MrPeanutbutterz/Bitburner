453059e9c04e11b2edf3b233e6615670dd7b219b
Charlie Eysbroek <C.eysbroek@hotmail.com>
2022-12-14 20:19:35 +0100

    first commit

---
+++ Genesis/Collect.js
@@ -0,0 +1,10 @@
+// Creator: https://github.com/MrPeanutbutterz 
+
+/** @param {NS} ns */
+export async function main(ns) {
+    var target = ns.args[0];
+    var delay = ns.args[1];
+
+    await ns.sleep(delay)
+    await ns.hack(target);
+}
